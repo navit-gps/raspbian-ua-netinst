c099d42d54dcdab454f04c21ce33f729980284c9
Pierre Grandin <grandinp@altern.org>
2017-01-11 22:24:01 -0800

    Build and install as 'navit' instead of 'pi'

--- build_navit.sh
+++ build_navit.sh
@@ -1,3 +1,5 @@
+apt-get install -y libgtk2.0-dev
+
 cd /usr/src
 git clone https://github.com/navit-gps/navit.git
 mkdir navit-bin
@@ -5,7 +7,9 @@ cd navit-bin/
 cmake  -DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/ ../navit/
 make -j4
 make install
-mkdir /home/pi/.navit/
-cp /usr/local/share/navit/navit.xml /home/pi/.navit/
-sed -i 's@gui type="internal" enabled="yes">@gui type="internal" enabled="yes" fullscreen="1">@' /home/pi/.navit/navit.xml
-chown -R pi /home/pi/
+cp navit/maps/*.xml /usr/local/share/navit/maps/
+mkdir /home/navit/.navit/
+cp /usr/local/share/navit/navit.xml /home/navit/.navit/
+sed -i 's@gui type="internal" enabled="yes">@gui type="internal" enabled="yes" fullscreen="1">@' /home/navit/.navit/navit.xml
+sed -i 's/zoom="256"/zoom="32"/' /home/navit/.navit/navit.xml
+chown -R navit /home/navit/
